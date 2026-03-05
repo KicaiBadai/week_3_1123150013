99228b51e711e93bc78922478a35c1e73bd7e033
KicaiBadai <adityamaula80@gmail.com>
2026-03-05 20:09:02 +0700

    11

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './core/routes/app_router.dart';
-import './features/cart/presentation/providers/cart_provider.dart'; // Path yang benar
+import './features/cart/presentation/providers/cart_provider.dart';
 import './features/cart/data/repositories/cart_repository_impl.dart';
 
 void main() {
@@ -22,10 +22,32 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Flutter E-Commerce',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          primaryColor: Colors.deepPurple,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.light,
+          ),
           useMaterial3: true,
+
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.deepPurple,
+          ),
+
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple,
+              foregroundColor: Colors.white,
+              minimumSize: const Size(double.infinity, 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
         ),
         initialRoute: AppRouter.catalog,
         routes: AppRouter.routes,
